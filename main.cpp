940ebe65389d1547593ebcff265c21e877772ab1
Przemysław Kazibudzki <68235865+przemyslawkazibudzki@users.noreply.github.com>
2021-12-08 13:20:18 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,37 +1,45 @@
-#include<iostream>
-
+#include <iostream>
 
 using namespace std;
 
-class Psy{
+class zwierzePies {
+    string imie;
+    string rasa;
+    int wiek;
 public:
-string imie;
-string rasa;
-int wiek;
-public:
-    void dodaj_psa(){
-        cout << "Podaj imie psa: ";
-        cin >> imie;
-        cout << "Podaj rase psa: ";
-        cin >> rasa;
-        cout << "Podaj wiek psa: ";
-        cin >> wiek;
-}
-    void pokazPsa(){
-    cout << "Twoj pies ma imie: "<< imie << ", jest rasy: "<< rasa << "i ma:"<< wiek << "lat. " << endl;
-    };
+    void przypisaniePieska() {
+    cout <<  "Podaj imie psa -> ";
+    cin >> imie;
+    cout << "Podaj rase pieska -> ";
+    cin >> rasa;
+    cout << "Zapodaj wiek twojego pieska -> ";
+    cin >> wiek;
+    }
+    void pokazPieska() {
+    cout << "Ten piesek o ktorym wspominales posiada imie: " << imie << ", jest rasy: " << rasa << " a wiek tego pieska wynosi " << wiek << " lat." << endl;
+    }
+    void wyrazyPieska(){
+    cout << "Ten piesek o imieniu " << imie << " jest zly i szczeka: HAUL HAU HAYU";
+    }
+};
 
-int main(){
+int main()
+{
+    int k;
+    cout << "Napisz ile psow mozesz napisac -> ";
+    cin >> k;
+    zwierzePies *l = new zwierzePies[k];
 
-    Psy pies1;
+    for (int i=0; i<k; i++)
+    {
+     l[i].przypisaniePieska();
+    }
 
-    pies1.dodajPsa();
-    pies1.pokazPsa();
-    Pies1.szczekanie();
-}
+    delete [] l;
 
-
-
-return 0;
-
+    zwierzePies piesiaw1;
+    piesiaw1.przypisaniePieska();
+    piesiaw1.pokazPieska();
+    piesiaw1.wyrazyPieska();
+    return 0;
 }
